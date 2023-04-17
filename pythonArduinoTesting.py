ec5be9637dcfb1b1b5213fa9c48cc9b71bd73fb7
YosefSokilik <111966410+YosefSokilik@users.noreply.github.com>
2023-04-17 09:24:37 +0300

    Update pythonArduinoTesting.py

--- pythonArduinoTesting.py
+++ pythonArduinoTesting.py
@@ -2,18 +2,37 @@ import serial
 import time
 
 
+def serial_connect(port_name):
+    try:
+        ser = serial.Serial(port_name, baudrate=9600, timeout=.1)
+        print("opened port " + ser.name + '\n')
+        # give arduino time to reset
+        time.sleep(2)
+        # flush input buffer, discarding all contents
+        ser.reset_input_buffer()
+        return ser
+    except serial.SerialException:
+        raise IOError("problem connecting to " + port_name)
+
+
+def set_voltage(ser, v_in):
+    v_in_str = v_in + '\n'
+    ser.write(v_in_str.encode())
+    return
+
+
+def get_voltage(ser):
+    v_out = int(ser.readline().rstrip().decode())
+    return v_out
+
+
 if __name__ == '__main__':
-    
-    
-    vin = 2.25 # float(input('Enter expected MCP4725 voltege:').strip())
-    arduino = serial.Serial(port = 'COM3',baudrate=115200, timeout=0)
-    serial.write(str(vin))
-    print("V in to arduino: %f", vin)
-    time.sleep(2) #stopes the nrunning progrram 2 sec
-    vout = float(serial.readline())
-    if vout == 3.25:
-        print("success")
-    else:
-        print("failure")
-        print("V out from arduino: %f", vout)
-   
+
+    port = 'COM3'
+    arduino = serial_connect(port)
+
+    set_voltage(arduino, '0012')  # We need to send a string with length of 4, because the Arduino expect receiving 4
+    # bytes.
+    time.sleep(0.1)
+    data = get_voltage(arduino)
+    print(data)
