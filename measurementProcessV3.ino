6caa7a1c0cf5e00bb8b2669ecb739d0e667844ac
shuvi pasko <110031174+shuvipas@users.noreply.github.com>
2023-05-15 10:12:13 +0300

    Update measurementProcessV3.ino

--- measurementProcessV3.ino
+++ measurementProcessV3.ino
@@ -1,67 +1,76 @@
 #include <Adafruit_MCP4725.h> // MCP4725 library from adafruit
 #define ON HIGH
 #define OFF LOW
+
 Adafruit_MCP4725 DAC; 
 const int analogVin = A0;
-//float voltageRead = 0;
-//const float vcc = 5.0;
-const int measurement_times = 5;
+const int DacMeasurement = A1;
+const int measurement_times = 2;
 const int DAC_res = 4096;
 const long reportInterval = 200; // How often to write the result to serial in milliseconds
-//const int HANDSHAKE = 0;
-//const int CASE_1 = 1;
+
+const int HANDSHAKE = 0;
+const int CASE_1 = 1;
 
 //depends on the digital pins but the values must be continues 
-const int R2 = 0;// 100 ohm
-const int R3 = 1; // 1000 ohm == 1 k*ohm
-const int R4 = 2; // 10000 ohm == 10 k*ohm
-const int R5 = 3; // 100000 ohm == 100 k*ohm
-const int R6 = 4; // 1000000 ohm == 1 M*ohm
+const int R2 = 2;// 100 ohm
+const int R3 = 3; // 1 K*ohm
+const int R4 = 4; // 10 K*ohm
+const int R5 = 5; // 100 K*ohm
+const int R6 = 6; // 1 M*ohm
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   DAC.begin(0x60); // Default I2C Address of MCP4725 
   Serial.setTimeout(1);
+  pinMode(R4, OUTPUT);
+  
+  /*
   pinMode(R2, OUTPUT);
   pinMode(R3, OUTPUT);
-  pinMode(R4, OUTPUT);
   pinMode(R5, OUTPUT);
   pinMode(R6, OUTPUT);
-  
+  */
 }
 
 void loop() {
-
 if (Serial.available() > 0) {
   int inByte = Serial.read();
   int test = 0;
-  if(inByte ==1) {test=1;}
-  else if (inByte ==0 &&Serial.availableForWrite())
-  {
+  if(inByte == CASE_1) {test = 1;}
+  else if (inByte == HANDSHAKE)
+     { 
+      if (Serial.availableForWrite())
+        {
     Serial.println("Message received.");
-    test=0;
+        }
+    test = 0;
   }
   
   if(test)
   {
-    for(int i=R2; i<=R6; i++)
+    /*
+    for(int i = R1; i <= R5; i++)
     {
-
+      
+      digitalWrite(R1, OFF);
       digitalWrite(R2, OFF);
       digitalWrite(R3, OFF);
       digitalWrite(R4, OFF);
       digitalWrite(R5, OFF);
-      digitalWrite(R6, OFF);
 
-      digitalWrite(i, ON);// turn on the specific ressitor
-
-
-      int DAC_value = 0;  // If it doesn't work we could try diffrent data types like 'uint32_t'     
-      while(DAC_value<DAC_res)
+      digitalWrite(i, ON);// turn on the specific resistor
+      */
+      digitalWrite(R4, ON);
+      int DAC_value[20] = {200, 400, 600, 800, 1000, 
+                           1200, 1400, 1600, 1800, 2000,
+                           2200, 2400, 2600, 2800, 3000,
+                           3200, 3400, 3600, 3800, 4000};  // If it doesn't work we could try diffrent data types like 'uint32_t'     
+      for(int j = 0; j < 20; j++)
       {  
 
-      DAC.setVoltage(DAC_value, false);  //setVoltage(value, storeflag(saves val for later use)) 
+      DAC.setVoltage(DAC_value[j], false);  //setVoltage(value, storeflag(saves val for later use)) 
       int measurement = 0;
       unsigned long timeLastWrite = millis();
 
@@ -72,19 +81,16 @@ if (Serial.available() > 0) {
           {
             timeLastWrite = currTime;
             int adcVal = analogRead(analogVin);
-            Serial.println(i); // what resistor is turned on R = 10^i
-            Serial.println(DAC_value); // the dac val 
+            int dacVal = analogRead(DacMeasurement);
+            Serial.println(R4);//i); // what resistor is turned on R = 10^i
+            Serial.println(dacVal);//DAC_value[j]); // the dac val 
             Serial.println(adcVal); 
             measurement++;
           }  
       }
-        
-        // add here a if the vin+vdut= 18v then break
-        // and the v_dut can be the mean 
-      DAC_value += 200;
-      } 
-    }
+     } 
+   // }//for(Res)
     Serial.println("Done");
   } //if(test)
-}//if (Serial.available() > 0)
+ }//if (Serial.available() > 0)
 }//void loop
