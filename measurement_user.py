7dda11277a5a3269beb82eedddc695e8536cf58b
shuvi pasko <110031174+shuvipas@users.noreply.github.com>
2023-06-07 12:01:57 +0300

    Update measurement_user.py

--- measurement_user.py
+++ measurement_user.py
@@ -1,7 +1,6 @@
 import serial
 import time
 import struct
-#  import matplotlib.pyplot as plt
 import openpyxl
 
 # DAC_RESOLUTION = 4096
@@ -13,8 +12,8 @@ V_SUPPLY = 18
 SWEEP = 1
 USER_INPUT = 2
 END_PROGRAM = "Done"
-# The V.D. of the InAmp
-R1 = 47.1
+
+R1 = 47.1 # The voltege devider of the InAmp
 R2 = 10.015 + 6.796
 
 
@@ -39,9 +38,6 @@ def handshake_arduino(ser, sleep_time=1, print_handshake_message=True, handshake
     ser.open()
     # Chill out while everything gets set
     time.sleep(sleep_time)
-    # Set a long timeout to complete handshake
-    # timeout = ser.timeout
-    # ser.timeout = 2
     # Read and discard everything that may be in the input buffer
     _ = ser.read_all()
     # Send request to Arduino
@@ -56,23 +52,11 @@ def handshake_arduino(ser, sleep_time=1, print_handshake_message=True, handshake
     if print_handshake_message:
         print("Handshake message: " + handshake_message.decode())
 
-    # Reset the timeout
-    # ser.timeout = timeout
-
 
 def get_adc_voltage(ser):
     adc_num = int(ser.read_until().rstrip().decode())
     v_adc = (adc_num * VCC) / ADC_RESOLUTION
     return v_adc
-
-
-'''
-def get_dac_voltage(ser):
-    dac_num = int(ser.read_until().rstrip().decode())
-    v_dac = (dac_num * VCC) / DAC_RESOLUTION
-    return v_dac
-'''
-
 
 def get_resistor(ser):
     res = ser.read_until().rstrip().decode()
@@ -81,33 +65,6 @@ def get_resistor(ser):
     res = 10 ** int(res)
     return res
 
-
-'''
-def remove_data(data_list, measurement_time):
-    i = 0
-    diff_points = len(data_list) / measurement_time
-    while i < diff_points - 1:
-        diff = data_list[(i + 1) * measurement_time][0] - data_list[i * measurement_time][0]
-        if diff < VCC / ADC_RESOLUTION:
-            del data_list[(i + 1) * measurement_time:(i + 1) * measurement_time + measurement_time]
-        else:
-            i += 1
-    return data_list
-'''
-
-'''
-def graph_plot(data):
-    # split tuples into separate lists
-    v_out, v_in, cur, r = zip(*data)
-
-    # plot x against y
-    plt.scatter(v_out, cur)
-    plt.plot(v_out, cur)
-    plt.xlabel('Voltage [V]')
-    plt.ylabel('cur [A]')
-    plt.title('V/I Curve')
-    plt.show()
-'''
 
 def convert_list_to_excel(data_list):
     head_line = ("v_adc", "v_dac", "current", "dut_res")
@@ -120,13 +77,6 @@ def convert_list_to_excel(data_list):
         sheet.append(row_data)
 
     workbook.save(file_path)
-
-
-'''
-def calibration(ser):
-    adc_num = int(ser.read_until().rstrip().decode())
-    vcc = adc_num / ADC_RESOLUTION  # * ((r2+ r1) / r1)
-'''
 
 
 def reconnect(ser):
@@ -267,4 +217,4 @@ if __name__ == '__main__':
 
     arduino.close()
 
-    # graph_plot(data)
+
